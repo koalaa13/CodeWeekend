6af5811c26f386c5b31ce0a4ab2f96edbff22901
alex-shishkin <alex-shishkin@yandex-team.ru>
2024-06-08 20:51:42 +0300

    New solver

--- src/main/java/org/example/solve/impl/CleverSolver.java
+++ src/main/java/org/example/solve/impl/CleverSolver.java
@@ -81,7 +81,6 @@ public class CleverSolver extends Solver {
         for (int iter = 0; iter < movesLimit; iter++) {
             List<State> allStates = answers.get(iter).getAllStates();
             if (iter % 20 == 0) {
-                System.out.println();
                 System.out.println("Iter: " + (iter + 1) + "/" + movesLimit + ". Max gold: " +
                         allStates.stream()
                                 .map(s -> s.game.getGoldGained()).max(Comparator.naturalOrder())
